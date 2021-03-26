46d15421e7ac7072fe967c5bea896c128155bbc0
GitHub Action <action@github.com>
2021-03-26 16:04:39 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/coderknocksanchan/knocktool/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.coderknocksanchan.knocktool.services
+
+import com.github.coderknocksanchan.knocktool.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
